7bffa62299b5bafb333b6402fa663e53c74879bc
bblomqui <bblomqui@macalester.edu>
2020-03-13 11:24:40 -0500

    Basic farm

--- Movement/GameScene.swift
+++ Movement/GameScene.swift
@@ -6,82 +6,77 @@
 //  Copyright © 2020 Benjamin Blomquist. All rights reserved.
 //
 
+
+
 import SpriteKit
 import GameplayKit
+import AVFoundation
+
+class BG: SKSpriteNode{ }
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var lemonTree = SKSpriteNode()
+    var appleTree = SKSpriteNode()
+    var bananaTree = SKSpriteNode()
+    var bombSoundEffect: AVAudioPlayer?
+    var soundEffect: AVAudioPlayer?
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //foo
+        let background = SKSpriteNode (imageNamed: "farm bg")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.zPosition = -1
+        addChild(background)
+        
+        appleTree = SKSpriteNode(imageNamed: "apple tree")
+        appleTree.position = CGPoint(x: frame.midX + 400, y: frame.midY - 200)
+        addChild(appleTree)
+        
+        bananaTree = SKSpriteNode(imageNamed: "banana tree")
+        bananaTree.position = CGPoint(x: frame.midX + 100, y: frame.midY - 200)
+        addChild(bananaTree)
+        self.anchorPoint = CGPoint(x:0.5,y:0.5)
+        
+        lemonTree = SKSpriteNode(imageNamed: "lemon tree")
+        lemonTree.position = CGPoint(x: frame.midX + 250, y: frame.midY - 100)
+        addChild(lemonTree)
+        
+        
+        let path = Bundle.main.path(forResource: "bgm.mp3", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            // couldn't load file :(
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        let path2 = Bundle.main.path(forResource: "Dog bark.wav", ofType: nil)!
+        let url2 = URL(fileURLWithPath: path2)
+
+        do {
+            soundEffect = try AVAudioPlayer(contentsOf: url2)
+            soundEffect?.play()
+        } catch {
+            // couldn't load file :(
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            
+            let location = touch.location(in: self)
+            
+            bananaTree.position.x = location.x
+            bananaTree.position.y = location.y
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
