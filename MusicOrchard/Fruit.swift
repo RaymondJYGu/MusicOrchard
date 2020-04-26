2475584bfcca0c819c69f870b7cde7eb4a97c359
bblomqui1 <bblomqui1@macalester.edu>
2020-04-26 16:45:16 -0500

    Fruit and Tree classes

--- MusicOrchard/Fruit.swift
+++ MusicOrchard/Fruit.swift
@@ -1,52 +1,52 @@
 //
-//  Tree.swift
+//  Fruit.swift
 //  MusicOrchard
 //
-//  Created by Chenxin Zhu on 4/19/20.
+//  Created by Chenxin Zhu on 4/23/20.
 //  Copyright © 2020 Benjamin Blomquist. All rights reserved.
 //
 
 import Foundation
 import SpriteKit
 
-class Tree {
+class Fruit {
     var filename: String
     var node: SKSpriteNode
-    var fruits: [Fruit]
     var width: Double
     var height: Double
     var xPosition: Double
     var yPosition: Double
+    var musicName: String
+    var musicAction: SKAction
     
     init(filename: String) {
         node = SKSpriteNode(imageNamed: filename)
         self.filename = filename
-        fruits = []
         width = 0
         height = 0
         xPosition = 0
         yPosition = 0
+        musicName = ""
+        musicAction = SKAction()
     }
     
-    init(filename: String, width: Double, height: Double, xPosition: Double, yPosition: Double) {
+    init(filename: String, width: Double, height: Double, xPosition: Double, yPosition: Double, musicName: String) {
         node = SKSpriteNode(imageNamed: filename)
         self.filename = filename
-        fruits = []
         self.width = 0
         self.height = 0
         self.xPosition = 0
         self.yPosition = 0
+        self.musicName = musicName
+        musicAction = SKAction.playSoundFileNamed(musicName, waitForCompletion: false)
         setSize(width: width, height: height)
         setPosition(xPosition: xPosition, yPosition: yPosition)
     }
     
-    func addFruit(fruit: Fruit, width: Double, height: Double, xPosition: Double, yPosition: Double) {
-        fruit.setSize(width: width, height: height)
-        fruit.setPosition(xPosition: xPosition, yPosition: yPosition)
-        fruit.getNode().zPosition = getNode().zPosition + 1
-        node.addChild(fruit.getNode())
-        fruits.append(fruit)
+    func playMusic() {
+        node.run(musicAction)
     }
+    
     
     func setSize(width: Double, height: Double) {
         node.scale(to: CGSize(width: width, height: height))
@@ -60,6 +60,11 @@ class Tree {
         node.position = CGPoint(x: xPosition, y: yPosition)
     }
 
+    func setMusic(musicName: String) {
+        self.musicName = musicName
+        musicAction = SKAction.playSoundFileNamed(musicName, waitForCompletion: false)
+    }
+    
     func getNode() -> SKSpriteNode {
         return node
     }
