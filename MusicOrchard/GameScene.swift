110f764274b981bc8e06c1945023ca7da4458cc9
bblomqui <bblomqui@macalester.edu>
2020-05-07 14:40:47 -0700

    Update GameScene.swift
    Added 3rd fruit to each tree

--- MusicOrchard/GameScene.swift
+++ MusicOrchard/GameScene.swift
@@ -23,22 +23,27 @@ class GameScene: SKScene {
     var appleTree = Tree(filename: "appleTree")
     var apple = Fruit(image: "apple", instrument: "banjo")
     var apple2 = Fruit(image: "apple", instrument: "banjo")
+    var apple3 = Fruit(image: "apple", instrument: "banjo")
     
     var bananaTree = Tree(filename: "bananaTree")
     var banana = Fruit(image: "banana", instrument: "voice")
     var banana2 = Fruit(image: "banana", instrument: "voice")
+    var banana3 = Fruit(image: "banana", instrument: "voice")
     
     var coconutTree = Tree(filename: "coconutTree")
     var coconut = Fruit(image: "coconut", instrument: "cello")
     var coconut2 = Fruit(image: "coconut", instrument: "cello")
+    var coconut3 = Fruit(image: "coconut", instrument: "cello")
     
     var lemonTree = Tree(filename: "lemonTree")
     var lemon = Fruit(image: "lemon", instrument: "trumpet")
     var lemon2 = Fruit(image: "lemon", instrument: "trumpet")
+    var lemon3 = Fruit(image: "lemon", instrument: "trumpet")
     
     var peachTree = Tree(filename: "peachTree")
     var peach = Fruit(image: "peach", instrument: "sax")
     var peach2 = Fruit(image: "peach", instrument: "sax")
+    var peach3 = Fruit(image: "peach", instrument: "sax")
     
     override func didMove(to view: SKView) {
         //foo
@@ -49,30 +54,35 @@ class GameScene: SKScene {
         
         appleTree.grow(fruit: apple)
         appleTree.grow(fruit: apple2)
+        appleTree.grow(fruit: apple3)
         appleTree.node.setScale(GameScene.imageScale)
         addChild(appleTree.node)
         appleTree.setPosition(xPosition: Double(frame.midX + 400), yPosition: Double(frame.midY - 100))
         
         bananaTree.grow(fruit: banana)
         bananaTree.grow(fruit: banana2)
+        bananaTree.grow(fruit: banana3)
         bananaTree.node.setScale(GameScene.imageScale)
         addChild(bananaTree.node)
         bananaTree.setPosition(xPosition: Double(frame.midX + 50), yPosition: Double(frame.midY - 150))
         
         coconutTree.grow(fruit: coconut)
         coconutTree.grow(fruit: coconut2)
+        coconutTree.grow(fruit: coconut3)
         coconutTree.node.setScale(GameScene.imageScale)
         addChild(coconutTree.node)
         coconutTree.setPosition(xPosition: 230, yPosition: 200)
         
         lemonTree.grow(fruit: lemon)
         lemonTree.grow(fruit: lemon2)
+        lemonTree.grow(fruit: lemon3)
         lemonTree.node.setScale(GameScene.imageScale)
         addChild(lemonTree.node)
         lemonTree.setPosition(xPosition: Double(frame.midX - 350), yPosition: Double(frame.midY - 200))
         
         peachTree.grow(fruit: peach)
         peachTree.grow(fruit: peach2)
+        peachTree.grow(fruit: peach3)
         peachTree.node.setScale(GameScene.imageScale)
         addChild(peachTree.node)
         peachTree.setPosition(xPosition: -260, yPosition: 165)
