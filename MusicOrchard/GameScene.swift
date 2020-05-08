9931863ac0196366919b1581db743baf76f017e6
Juliette <juliette.parmenterr@gmail.com>
2020-05-07 22:26:50 -0400

    small change: got rid of all "frame.midX" and "frame.mixY" relative coordinates

--- MusicOrchard/GameScene.swift
+++ MusicOrchard/GameScene.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2020 Benjamin Blomquist. All rights reserved.
 //
 
-
-
 import SpriteKit
 import GameplayKit
 import AVFoundation
@@ -46,9 +44,9 @@ class GameScene: SKScene {
     var peach3 = Fruit(image: "peach", instrument: "sax")
     
     override func didMove(to view: SKView) {
-        //foo
+        
         let background = SKSpriteNode (imageNamed: "farm bg")
-        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.position = CGPoint(x: 0, y: 0)
         background.zPosition = -1
         addChild(background)
         
@@ -58,37 +56,53 @@ class GameScene: SKScene {
         let lemonRoot = SKSpriteNode (imageNamed: "hole1")
         let peachRoot = SKSpriteNode (imageNamed: "hole1")
         
+        appleTree.node.addChild(appleRoot)
+        appleRoot.position = CGPoint(x: 10, y: -650)
+        appleRoot.setScale(0.7)
         
+        bananaTree.node.addChild(bananaRoot)
+        bananaRoot.position = CGPoint(x: -40, y: -740)
+        bananaRoot.setScale(0.7)
         
+        coconutTree.node.addChild(coconutRoot)
+        coconutRoot.position = CGPoint(x: -30, y: -730)
+        coconutRoot.setScale(0.7)
         
+        lemonTree.node.addChild(lemonRoot)
+        lemonRoot.position = CGPoint(x: 10, y: -680)
+        lemonRoot.setScale(0.7)
+        
+        peachTree.node.addChild(peachRoot)
+        peachRoot.position = CGPoint(x: 10, y: -565)
+        peachRoot.setScale(0.7)
         
         appleTree.grow(fruit: apple)
         appleTree.grow(fruit: apple2)
         appleTree.grow(fruit: apple3)
         appleTree.node.setScale(GameScene.imageScale)
         addChild(appleTree.node)
-        appleTree.setPosition(xPosition: Double(frame.midX + 400), yPosition: Double(frame.midY - 100))
+        appleTree.setPosition(xPosition: 400, yPosition: -100)
         
         bananaTree.grow(fruit: banana)
         bananaTree.grow(fruit: banana2)
         bananaTree.grow(fruit: banana3)
         bananaTree.node.setScale(GameScene.imageScale)
         addChild(bananaTree.node)
-        bananaTree.setPosition(xPosition: Double(frame.midX + 50), yPosition: Double(frame.midY - 150))
+        bananaTree.setPosition(xPosition: 50, yPosition: -150)
         
         coconutTree.grow(fruit: coconut)
         coconutTree.grow(fruit: coconut2)
         coconutTree.grow(fruit: coconut3)
         coconutTree.node.setScale(GameScene.imageScale)
         addChild(coconutTree.node)
-        coconutTree.setPosition(xPosition: 230, yPosition: 200)
+        coconutTree.setPosition(xPosition: 230, yPosition: 190)
         
         lemonTree.grow(fruit: lemon)
         lemonTree.grow(fruit: lemon2)
         lemonTree.grow(fruit: lemon3)
         lemonTree.node.setScale(GameScene.imageScale)
         addChild(lemonTree.node)
-        lemonTree.setPosition(xPosition: Double(frame.midX - 350), yPosition: Double(frame.midY - 150))
+        lemonTree.setPosition(xPosition: -350, yPosition: -160)
         
         peachTree.grow(fruit: peach)
         peachTree.grow(fruit: peach2)
@@ -96,38 +110,9 @@ class GameScene: SKScene {
         peachTree.node.setScale(GameScene.imageScale)
         addChild(peachTree.node)
         peachTree.setPosition(xPosition: -200, yPosition: 165)
-        
-        appleTree.node.addChild(appleRoot)
-        appleRoot.position = CGPoint(x: frame.midX+10, y: frame.midY-650)
-        appleRoot.setScale(0.7)
-        
-        bananaTree.node.addChild(bananaRoot)
-        bananaRoot.position = CGPoint(x: frame.midX-30, y: frame.midY-740)
-        bananaRoot.setScale(0.7)
-        
-        coconutTree.node.addChild(coconutRoot)
-        coconutRoot.position = CGPoint(x: frame.midX-30, y: frame.midY-730)
-        coconutRoot.setScale(0.7)
-        
-        lemonTree.node.addChild(lemonRoot)
-        lemonRoot.position = CGPoint(x: frame.midX+15, y: frame.midY-710)
-        lemonRoot.setScale(0.7)
-        
-        peachTree.node.addChild(peachRoot)
-        peachRoot.position = CGPoint(x: frame.midX+10, y: frame.midY-570)
-        peachRoot.setScale(0.7)
-        
-        
     }
-
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
 }
-
-    // You might end up writing other SKAction music methods like this:
-    //
-    // let appleRhythm = SKAction.rhythm(instrument: "voice", length: 5, pulse: 0.1)
-
-
